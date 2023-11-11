afbe562254aecb69f0244f95f0aa9ce3b8b9b19c
JD_Rodrigues <jdevproject01@gmail.com>
2023-11-11 09:44:05 -0300

    feat: implements CourseFactory

---
+++ database/factories/CourseFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Generator as Faker;
+
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course>
+ */
+class CourseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     * 
+     */
+    protected $model = Course::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->sentence(),
+            'description' => fake()->text(),
+            'image' => fake()->imageUrl()
+        ];
+    }
+}
