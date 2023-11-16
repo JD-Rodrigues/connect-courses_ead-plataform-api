fffb25b3eeacbd7138f9c1d366c200284cb82ba1
JD_Rodrigues <jdevproject01@gmail.com>
2023-11-15 21:34:07 -0300

    feat: implements Module model

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Traits\UuidTrait;
 
 class Module extends Model
 {
-    use HasFactory;
+    use HasFactory, UuidTrait;
+
+    public $incrementing = false;
+    protected $keyTypes = 'uuid';
+    protected $fillable = ['id', 'course_id', 'name'];
+
 }
