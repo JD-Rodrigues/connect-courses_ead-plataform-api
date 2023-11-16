d78ad638cf383433f2186bafa38b5fd36f4f6015
JD_Rodrigues <jdevproject01@gmail.com>
2023-11-15 21:36:01 -0300

    feat: implements ModuleRepository->getAllModules()

--- app/Repositories/ModuleRepository.php
+++ app/Repositories/ModuleRepository.php
@@ -6,5 +6,19 @@ use App\Models\Module;
 
 class ModuleRepository
 {
-   
+    static function getAllModules($id) {
+        try {
+            return Module::where('course_id', $id)->get();
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+
+    static function getModule($id) {        
+        try {
+            
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
 }
