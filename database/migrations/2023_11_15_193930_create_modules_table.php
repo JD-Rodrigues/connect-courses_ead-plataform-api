f3251e59fef9d19ceefcf72701a3b0e0d9fa21f6
JD_Rodrigues <jdevproject01@gmail.com>
2023-11-15 21:33:31 -0300

    feat: implements create_table_modules migration

--- database/migrations/2023_11_15_193930_create_modules_table.php
+++ database/migrations/2023_11_15_193930_create_modules_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('modules', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->uuid('course_id')->nullable(false);
+            $table->string('name');
             $table->timestamps();
         });
     }
