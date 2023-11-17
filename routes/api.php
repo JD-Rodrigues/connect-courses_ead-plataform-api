7a3704d6d1273d5677a73be5815e3c19d14c3443
JD_Rodrigues <jdevproject01@gmail.com>
2023-11-17 12:05:38 -0300

    feat: implements GET /modules/{id}/lessons and /lessons/{id} routes

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use  App\Http\Resources\CourseResource;
 use App\Http\Controllers\ModuleController;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\LessonController;
 
 
 
@@ -16,3 +17,5 @@ Route::get('/courses', [CourseController::class, 'index']);
 
 Route::get('/courses/{id}', [CourseController::class, 'show']);
 Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
+Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
+Route::get('/lessons/{id}', [LessonController::class, 'show']);
