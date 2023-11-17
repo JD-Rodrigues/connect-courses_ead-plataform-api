8e40fed4dfa3b83cd7438b45e0eb93f9b150155a
JD_Rodrigues <jdevproject01@gmail.com>
2023-11-17 17:40:36 -0300

    feat: create Support model and its artifacts

---
+++ app/Repositories/SupportRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Support;
+
+class SupportRepository
+{
+    static function getAllSupports($id) {
+        try {
+            
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+
+    static function getSupport($id) {        
+        try {
+            
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+}
