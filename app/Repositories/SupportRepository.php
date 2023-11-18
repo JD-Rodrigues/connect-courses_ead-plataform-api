ae58f086a1da0be8219e07aedcddd883b6aa6755
JD_Rodrigues <jdevproject01@gmail.com>
2023-11-17 21:10:11 -0300

    feat: implements SupportRepository

--- app/Repositories/SupportRepository.php
+++ app/Repositories/SupportRepository.php
@@ -3,12 +3,13 @@
 namespace App\Repositories;
 
 use App\Models\Support;
+use App\Models\User;
 
 class SupportRepository
 {
-    static function getAllSupports($id) {
+    public function getAllSupports() {
         try {
-            
+            return $this->getLoggedUser()->supports;
         } catch (\Throwable $th) {
             return $th->getMessage();
         }
@@ -20,5 +21,9 @@ class SupportRepository
         } catch (\Throwable $th) {
             return $th->getMessage();
         }
+    }
+
+    private function getLoggedUser() {
+        return User::first();
     }
 }
