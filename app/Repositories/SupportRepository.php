67e358cb407370bad7e57e91c317204f9107726b
JD_Rodrigues <jdevproject01@gmail.com>
2023-11-28 18:47:50 -0300

    refactor: changes SupportRepository->getAllSupports() to show all users' supports

--- app/Repositories/SupportRepository.php
+++ app/Repositories/SupportRepository.php
@@ -10,26 +10,36 @@ class SupportRepository
 {
     use GetAuthUserTrait;
 
+    private $entity;
+
+    public function __construct(Support $model) {
+        $this->entity = $model;
+    }
+
+    
+
     public function getAllSupports(array $filters) {
         try {
-            return $this->getLoggedUser()
-            ->supports()
-            ->where(function($query)use($filters){
-                if (isset($filters['lesson_id'])) {
-                    $query->where('lesson_id', $filters['lesson_id']);
-                }
-                
-                if (isset($filters['status_code'])) {
-                    $query->where('status_code', $filters['status_code']);
-                }
-                if (isset($filters['description'])) {
-                    $filter = $filters['description'];
-                    $query->where("description", "LIKE", "%{$filter}%");
-                }
-
-                
-            })
-            ->get();
+            return $this->entity
+                ->where(function($query)use($filters){
+                    if (isset($filters['lesson_id'])) {
+                        $query->where('lesson_id', $filters['lesson_id']);
+                    }
+                    
+                    if (isset($filters['status_code'])) {
+                        $query->where('status_code', $filters['status_code']);
+                    }
+                    if (isset($filters['description'])) {
+                        $filter = $filters['description'];
+                        $query->where("description", "LIKE", "%{$filter}%");
+                    } 
+                    
+                    if (isset($filters['user'])) {
+                        $user = $this->getLoggedUser();
+                        $query->where('user_id', $user->id);
+                    }
+                })
+                ->get();
         } catch (\Throwable $th) {
             return $th->getMessage();
         }
