68e4f76d66d6e3bc01d6f88de6a565197fc81691
JD_Rodrigues <jdevproject01@gmail.com>
2023-11-29 14:08:55 -0300

    feat: implements AuthRepository->authenticate()

---
+++ app/Repositories/AuthRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Support;
+use App\Models\User;
+use App\Models\Traits\GetAuthUserTrait;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthRepository
+{
+    use GetAuthUserTrait;
+
+    private $entity;
+
+    public function __construct(User $model) {
+        $this->entity = $model;
+    }
+
+    public function getAuthUser(array $filters) {
+        
+    }
+
+    public function authenticate(array $credentials) {
+        try {
+            
+            $user = $this->entity->where('email', $credentials['email'])->first();
+        
+            if(!$user || !Hash::check($credentials['password'], $user->password)) {
+                throw ValidationException::withMessages([
+                    'email' => ['The provided credentials are incorrect.'],
+                ]);
+            }
+
+            $user->tokens()->delete();
+
+            return response()->json([
+                'token'=> $user->createToken($credentials['password'])->plainTextToken
+            ]);
+
+        } catch (\Throwable $th) {
+            return $th;
+        }
+    }
+}
