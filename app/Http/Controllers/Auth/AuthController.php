e6750410ae4a216954589f1f631f13c847367d44
JD_Rodrigues <jdevproject01@gmail.com>
2023-11-29 16:54:31 -0300

    feat: implements AuthController->getAuthenticatedUser()

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\AuthRequest;
 use App\Models\User;
 use App\Repositories\AuthRepository;
+use App\Http\Resources\AuthResource;
 
 class AuthController extends Controller
 {
@@ -15,13 +16,16 @@ class AuthController extends Controller
         $this->repository = $repository;
     }
 
+    public function getAuthenticatedUser() {
+        $user = $this->repository->getAuthUser();
+        return AuthResource::make($user);
+    }
+
     public function authenticateUser(AuthRequest $request) {
         return $this->repository->authenticate($request->validated());
     }
 
     public function logoutUser() {
         return $this->repository->logout();
-    }
-
-    
+    }    
 }
