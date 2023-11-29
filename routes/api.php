c65ba59064749220d5280b86c2cd5eb602cacf2a
JD_Rodrigues <jdevproject01@gmail.com>
2023-11-28 23:43:04 -0300

    feat: implements GET /login route and protects routes usein auth:sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -8,20 +8,22 @@ use App\Http\Controllers\CourseController;
 use App\Http\Controllers\LessonController;
 use App\Http\Controllers\SupportController;
 use App\Http\Controllers\SupportReplyController;
-
-
+use App\Http\Controllers\Auth\AuthController;
 
 Route::get('/', function() {
     return 'Aqui começa nossa API';
 });
 
-Route::get('/courses', [CourseController::class, 'index']);
+Route::post('/login', [AuthController::class, 'auth']);
 
-Route::get('/courses/{id}', [CourseController::class, 'show']);
-Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
-Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
-Route::get('/lessons/{id}', [LessonController::class, 'show']);
-Route::get('/supports', [SupportController::class, 'index']);
-Route::get('/my-supports', [SupportController::class, 'showMySupports']);
-Route::post('/supports', [SupportController::class, 'store']);
-Route::post('/support-replies', [SupportReplyController::class, 'store']);
+Route::middleware(['auth:sanctum'])->group(function() {
+    Route::get('/courses', [CourseController::class, 'index']);
+    Route::get('/courses/{id}', [CourseController::class, 'show']);
+    Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
+    Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
+    Route::get('/lessons/{id}', [LessonController::class, 'show']);
+    Route::get('/supports', [SupportController::class, 'index']);
+    Route::get('/my-supports', [SupportController::class, 'showMySupports']);
+    Route::post('/supports', [SupportController::class, 'store']);
+    Route::post('/support-replies', [SupportReplyController::class, 'store']);
+}); 
