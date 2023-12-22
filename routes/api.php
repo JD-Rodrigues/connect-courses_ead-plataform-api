b051f2372d2d4a8985c0480a052e009026822cf9
Domingos Rodrigues <domingosrodrigues.dev@gmail.com>
2023-12-21 23:29:45 -0300

    feat: implements POST /forgot-password route

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use  App\Http\Resources\CourseResource;
 use App\Http\Controllers\ModuleController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\LessonController;
 use App\Http\Controllers\SupportController;
 use App\Http\Controllers\SupportReplyController;
 use App\Http\Controllers\Auth\AuthController;
+use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
 
 Route::get('/', function() {
     return 'Aqui começa nossa API';
@@ -29,3 +33,16 @@ Route::middleware(['auth:sanctum'])->group(function() {
     Route::post('/support-replies', [SupportReplyController::class, 'store']);
     Route::post('/logout', [AuthController::class, 'logoutUser']);
 }); 
+
+Route::post('/forgot-password', function (Request $request) {
+    $request->validate(['email' => 'required|email']);
+ 
+    $status = Password::sendResetLink(
+        $request->only('email')
+    );
+ 
+    return $status === Password::RESET_LINK_SENT
+                ? "Link de redefinição de senha enviado para o e-mail $request->input('email)."
+                : "Houve um erro: $status";
+})->middleware('guest')->name('password.reset');
+
