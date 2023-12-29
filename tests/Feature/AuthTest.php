14545056e2cea762638ba8f8e9d92dc671343a01
Domingos Rodrigues <domingosrodrigues.dev@gmail.com>
2023-12-29 00:37:22 -0300

    feat: implements test_authorized_login() test in AuthTest

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -15,4 +15,25 @@ class AuthTest extends TestCase
 
         $response->assertStatus(422);
     }
+
+    public function test_authorized_login(): void
+    {
+        User::factory()->create([
+            'email'=> 'contato@eu.com',
+            'password'=> 'minhasenha',
+        ]);
+
+        // var_dump($user);
+        $response = $this->postJson(
+            '/login', 
+            [
+                'email' => 'contato@eu.com',
+                'password' => 'minhasenha',
+            ]
+        );
+
+        $response->assertJsonStructure(['token']);
+        $response->assertStatus(200);        
+        
+    }
 }
