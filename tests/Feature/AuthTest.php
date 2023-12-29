e8b6a3818ab58bb5653127ef6372641e8a0cda7a
Domingos Rodrigues <domingosrodrigues.dev@gmail.com>
2023-12-29 14:10:44 -0300

    refactor: change tests in AuthTest class to use AuthUtilsTrait->createAuthHeaders() method

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -5,10 +5,12 @@ namespace Tests\Feature;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Tests\Feature\Traits\AuthUtilsTrait;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
+    use AuthUtilsTrait;
     public function test_login_with_invalid_credentials(): void
     {
         $response = $this->postJson('/login', []);
@@ -37,42 +39,30 @@ class AuthTest extends TestCase
         
     }
 
-    public function test_logout_without_authentication(): void
+    public function test_unauthorized_logout_without_authentication(): void
     {
         $response = $this->postJson('/logout', []);
 
         $response->assertStatus(401);
     }
 
-    public function test_logout_with_authentication(): void
-    {
-        $user = User::factory()->create();
-
-        $token =  $user->createToken('password')->plainTextToken;
-        
-        $response = $this->postJson('/logout', [], [
-            'Authorization' => "Bearer {$token}"
-        ]);
+    public function test_success_logout_with_authentication(): void
+    {            
+        $response = $this->postJson('/logout', [], $this->createAuthHeader());
 
         $response->assertStatus(200);
     }
 
-    public function test_get_me_route_without_authentication(): void
+    public function test_unauthorized_get_me_without_authentication(): void
     {
         $response = $this->getJson("/me");
 
         $response->assertStatus(401);
     }
 
-    public function test_get_me_route_with_authentication(): void
-    {
-        $user = User::factory()->create();
-
-        $token =  $user->createToken('password')->plainTextToken;
-        
-        $response = $this->getJson('/me', [
-            'Authorization' => "Bearer {$token}"
-        ]);
+    public function test_success_get_me_with_authentication(): void
+    {        
+        $response = $this->getJson('/me', $this->createAuthHeader());
 
         $response->assertStatus(200);
     }
