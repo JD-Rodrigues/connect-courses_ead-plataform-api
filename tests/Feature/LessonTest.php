f557ebc69a54f04afe2895096e67f22c6a5c7763
Domingos Rodrigues <domingosrodrigues.dev@gmail.com>
2023-12-30 02:07:27 -0300

    test: get_a_lesson_without_authentication_fails()

--- tests/Feature/LessonTest.php
+++ tests/Feature/LessonTest.php
@@ -2,19 +2,25 @@
 
 namespace Tests\Feature;
 
+use App\Models\Course;
+use App\Models\Lesson;
+use App\Models\Module;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Tests\Feature\Traits\AuthUtilsTrait;
 use Tests\TestCase;
 
 class LessonTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use AuthUtilsTrait;
 
-        $response->assertStatus(200);
+    public function test_get_a_lesson_without_authentication_fails(): void
+    {
+        $lesson = Lesson::factory()->create();
+
+        $response = $this->getJson("/lessons/{$lesson->id}");
+
+        $response->assertStatus(401);
     }
+
 }
