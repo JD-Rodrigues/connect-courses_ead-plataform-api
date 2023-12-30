bca19a4e1f63b8aa8639e6c910affc93168f7a6a
Domingos Rodrigues <domingosrodrigues.dev@gmail.com>
2023-12-30 02:04:50 -0300

    test: get_a_module_without_authentication_fails()

--- tests/Feature/ModuleTest.php
+++ tests/Feature/ModuleTest.php
@@ -2,19 +2,27 @@
 
 namespace Tests\Feature;
 
+use App\Models\Course;
+use App\Models\Module;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Tests\Feature\Traits\AuthUtilsTrait;
 use Tests\TestCase;
 
 class ModuleTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use AuthUtilsTrait;
 
-        $response->assertStatus(200);
-    }
+    public function test_get_a_module_without_authentication_fails(): void
+    {
+        $course = Course::factory()->create();
+        $module = Module::factory()->create([
+            'course_id' => $course->id
+        ]);
+
+        $response = $this->getJson("/courses/{$course->id}/modules");
+
+        $response->assertStatus(401);
+    }   
+ 
 }
