4ad44a4378c0bba0f6ba1283083216d79bf9eed8
Domingos Rodrigues <domingosrodrigues.dev@gmail.com>
2023-12-30 15:08:04 -0300

    test: get_supports_without_authentication_fails()

--- tests/Feature/SupportTest.php
+++ tests/Feature/SupportTest.php
@@ -2,19 +2,22 @@
 
 namespace Tests\Feature;
 
+use App\Models\Lesson;
+use App\Models\Support;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Tests\Feature\Traits\AuthUtilsTrait;
 use Tests\TestCase;
 
 class SupportTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    use AuthUtilsTrait;
+    public function test_get_supports_without_authentication_fails(): void
     {
-        $response = $this->get('/');
+        $response = $this->getJson('/supports');
 
-        $response->assertStatus(200);
+        $response->assertStatus(401);
     }
+
+    
 }
