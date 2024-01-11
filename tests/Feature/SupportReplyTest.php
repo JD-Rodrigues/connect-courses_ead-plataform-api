d320b0ecc0a5092569b68ba23c53e7aa389c62d3
Domingos Rodrigues <domingosrodrigues.dev@gmail.com>
2024-01-11 03:14:35 -0300

    test: post_support_reply_without_authentication_fails

--- tests/Feature/SupportReplyTest.php
+++ tests/Feature/SupportReplyTest.php
@@ -18,33 +18,7 @@ class SupportReplyTest extends TestCase
         $response->assertStatus(401);
     }
 
-    public function test_post_support_reply_with_authentication_succeed(): void
-    {
-        $support = Support::factory()->create();
-        
-        $response = $this->post('/support-replies',
-            [
-                'support_id' => $support->id,
-                'description' => 'Esta é uma resposta a uma mensagem de suporte.'
-            ],
-            $this->createAuthHeader()
-        );
+ 
 
-        $supportReply = SupportReply::first();
-        // dd($response);
-        
-        $response->assertStatus(201);
-        $response->assertJson([
-            'support_id' => $support->id,
-            'description' => 'Esta é uma resposta a uma mensagem de suporte.'
-        ]);
-    }
-
-    public function test_post_support_reply_with_invalid_data_fails(): void
-    {
-        
-        $response = $this->post('/support-replies',[], $this->createAuthHeader());
-        
-        $response->assertStatus(302);
-    }
+    
 }
