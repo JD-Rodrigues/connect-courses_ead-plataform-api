57d825a23fcbf66f51387bd4bc465efe4007ef26
Domingos Rodrigues <domingosrodrigues.dev@gmail.com>
2024-01-12 01:34:29 -0300

    refactor: adds logic to increment quantity to existing views in ViewRepository

--- app/Repositories/ViewRepository.php
+++ app/Repositories/ViewRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories;
 use App\Models\Traits\GetAuthUserTrait;
+use App\Models\View;
 
 class ViewRepository
 {
@@ -9,9 +10,19 @@ class ViewRepository
     
     public function createNewView(array $data) {
        try {
-            $this->getLoggedUser()
-            ->views()
-            ->create($data);
+            $user = auth()->user();
+            $lessonJustViewed = $user->views()->where('lesson_id', $data['lesson_id'])->first();
+
+            if($lessonJustViewed){
+               $lessonJustViewed->update(
+                    ['quant' => $lessonJustViewed->quant +1]
+               );
+            }else{
+               auth()->user()->views()->create(['lesson_id'=>$data['lesson_id']]);
+            }
+
+            return response('Visualização contabilizada!', 201);
+
        } catch (\Throwable $th) {
             return $th->getMessage();
        }
