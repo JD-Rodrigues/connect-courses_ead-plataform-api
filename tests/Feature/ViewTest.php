373f1b8b74db9aa42c1c222eb741552d91ac914c
Domingos Rodrigues <domingosrodrigues.dev@gmail.com>
2024-01-13 22:17:31 -0300

    test: mark_view_with_authentication_succeed

--- tests/Feature/ViewTest.php
+++ tests/Feature/ViewTest.php
@@ -18,5 +18,39 @@ class ViewTest extends TestCase
         $response->assertStatus(401);
     }
 
-    
+    public function test_mark_view_with_authentication_succeed(): void
+    {
+        $lesson = Lesson::factory()->create();
+        $lesson2 = Lesson::factory()->create();
+        
+        $response = $this->post(
+            '/views',
+            [
+                'lesson_id' => $lesson->id
+            ],
+            $this->createAuthHeader());
+
+        $this->post(
+            '/views',
+            [
+                'lesson_id' => $lesson->id
+            ]
+        );
+
+        $this->post(
+            '/views',
+            [
+                'lesson_id' => $lesson2->id
+            ]
+        );
+
+        // $views = count(auth()->user()->views()->where('lesson_id', $lesson->id)->get());
+        // print_r(['views'=>$views]);
+        $views = json_decode(auth()->user()->views()->where('lesson_id', $lesson->id)->get()->first()->quant);
+        // dd($views);
+        $response->assertStatus(201);
+        $this->assertEquals(2, $views);
+
+    }
+
 }
